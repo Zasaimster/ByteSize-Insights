25fa1ef51536deddf17214a4a7f83a58b5e66375
Mengan Wang <mengan@g.ucla.edu>
2023-11-10 15:14:35 -0800

    Frontend and User Authentication With Next.js (#39)
    * boilerplate frontend
    
    * removed some landing page components, and changed URLs
    
    * added new home route
    
    * #37 authentication with signup and login (#38)
    
    authentication with signup and login
    
    * user homepage with repo names, and buttons to subscribe/view
    
    ---------
    
    Co-authored-by: Tony Jiang <tonyjiang02@gmail.com>

---
+++ frontend/byte-size/components/layout/navbar.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import useScroll from "@/lib/hooks/use-scroll";
+import { useSignInModal } from "./sign-in-modal";
+import { useSignUpModal } from "./sign-up-modal"
+import UserDropdown from "./user-dropdown";
+import { Session } from "next-auth";
+
+export default function NavBar({ session }: { session: Session | null }) {
+  const { SignInModal, setShowSignInModal } = useSignInModal();
+  const { SignUpModal, setShowSignUpModal } = useSignUpModal();
+  const scrolled = useScroll(50);
+
+  return (
+    <>
+      <SignInModal />
+      <SignUpModal />
+      <div
+        className={`fixed top-0 w-full flex justify-center ${scrolled
+          ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
+          : "bg-white/0"
+          } z-30 transition-all`}
+      >
+        <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
+          <Link href="/" className="flex items-center font-display text-2xl">
+            <Image
+              src="/logo.png"
+              alt="Precedent logo"
+              width="30"
+              height="30"
+              className="mr-2 rounded-sm"
+            ></Image>
+            <p>Byte-Sized Insights</p>
+          </Link>
+          <div>
+            {session ? (
+              <UserDropdown session={session} />
+            ) : (
+              <button
+                className="rounded-full border border-black mr-3 bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
+                onClick={() => setShowSignInModal(true)}
+              >
+                Login
+              </button>
+            )}
+            {session ? (
+              <UserDropdown session={session} />
+            ) : (
+              <button
+                className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
+                onClick={() => setShowSignUpModal(true)}
+              >
+                Sign up
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
