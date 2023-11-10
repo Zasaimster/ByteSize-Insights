72ee0de76a74585008a633ba027ea0fd6488d105
Tony Jiang <tonyjiang02@gmail.com>
2023-11-10 13:45:37 -0800

    authentication with signup and login

--- frontend/byte-size/components/layout/navbar.tsx
+++ frontend/byte-size/components/layout/navbar.tsx
@@ -4,22 +4,24 @@ import Image from "next/image";
 import Link from "next/link";
 import useScroll from "@/lib/hooks/use-scroll";
 import { useSignInModal } from "./sign-in-modal";
+import { useSignUpModal } from "./sign-up-modal"
 import UserDropdown from "./user-dropdown";
 import { Session } from "next-auth";
 
 export default function NavBar({ session }: { session: Session | null }) {
   const { SignInModal, setShowSignInModal } = useSignInModal();
+  const { SignUpModal, setShowSignUpModal } = useSignUpModal();
   const scrolled = useScroll(50);
 
   return (
     <>
       <SignInModal />
+      <SignUpModal />
       <div
-        className={`fixed top-0 w-full flex justify-center ${
-          scrolled
-            ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
-            : "bg-white/0"
-        } z-30 transition-all`}
+        className={`fixed top-0 w-full flex justify-center ${scrolled
+          ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
+          : "bg-white/0"
+          } z-30 transition-all`}
       >
         <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
           <Link href="/" className="flex items-center font-display text-2xl">
@@ -37,10 +39,20 @@ export default function NavBar({ session }: { session: Session | null }) {
               <UserDropdown session={session} />
             ) : (
               <button
-                className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
+                className="rounded-full border border-black mr-3 bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
                 onClick={() => setShowSignInModal(true)}
               >
-                Sign In
+                Login
+              </button>
+            )}
+            {session ? (
+              <UserDropdown session={session} />
+            ) : (
+              <button
+                className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
+                onClick={() => setShowSignUpModal(true)}
+              >
+                Sign up
               </button>
             )}
           </div>
