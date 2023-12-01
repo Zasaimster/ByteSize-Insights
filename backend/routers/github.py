a38ad49120ea014855810412fd6c992d73cdf0ab
Aiden Szeto <66970460+aidenszeto@users.noreply.github.com>
2023-12-01 00:34:16 -0800

    User and repository database routes (#41)
    * database routes
    
    * route for subscribing to repo

---
+++ backend/routers/github.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter
+from datetime import date
+from bson import json_util
+from bson.objectid import ObjectId
+import pymongo
+import json
+
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
+
+router = APIRouter(prefix="/github", tags=["github"])
+
+URI = "mongodb+srv://cs130:7SBYtWrVqif1EzoR@cluster0.miyegq5.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(URI)
+db = client["ByteSize-Insights"]
+col = db["repositories"]
+
+@router.get("/getAllRepos")
+async def get_all_repos():
+    data = [x for x in col.find()]
+    return {"message": parse_json(data)}
+
+@router.get("/getRepo")
+async def get_repo(
+    repo_id: str,
+    start_time: date = None,
+    end_time: date = None
+):
+    data = col.find_one({"_id": ObjectId(repo_id)})
+    return {"message": parse_json(data)}
