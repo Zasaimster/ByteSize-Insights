8524a971545d700b036438a2168c0fc4d2112d29
Tony Jiang <tonyjiang02@gmail.com>
2023-12-02 16:38:20 -0800

    Authentication routing (#44)
    * boilerplate frontend
    
    * authentication with signup and login
    
    * Added auth with google
    
    * removed comments
    
    ---------
    
    Co-authored-by: Anan Wang <mengan.wang@gmail.com>
    Co-authored-by: Tony Jiang <tonyjiang@Tonys-MBP-2.lan>

--- frontend/byte-size/app/api/auth/[...nextauth]/route.ts
+++ frontend/byte-size/app/api/auth/[...nextauth]/route.ts
@@ -2,15 +2,18 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
+export const authOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: "678890271244-kc4v08rvfve0075dd8tpdprkkp0kflco.apps.googleusercontent.com",
+      clientSecret: "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",
     }),
   ],
+  callbacks: {
+  },
+  debug: true
 };
 
 const handler = NextAuth(authOptions);
