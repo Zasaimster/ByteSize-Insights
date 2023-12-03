e4a7b7fa42dff0708330c51995533d7464ba2193
Aiden Szeto <aidenszeto@g.ucla.edu>
2023-12-03 04:10:24 -0800

    Vercel config for backend CD

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,9 @@
+import uvicorn
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from routers import github
 from routers import users
 
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    # might want to change this to a designated list of allowed origins to limit who can call our endpoints
-    # but I doubt this should matter since this will be hosted on the VPN
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.include_router(github.router)
 app.include_router(users.router)
@@ -22,3 +12,6 @@ app.include_router(users.router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+if __name__ == "__main__":
+  uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
