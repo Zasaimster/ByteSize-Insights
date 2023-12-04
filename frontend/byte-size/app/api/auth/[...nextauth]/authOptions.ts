4b994dc4089949d0c26d5472ac68599f4a378a00
Tony Jiang <tonyjiang02@gmail.com>
2023-12-04 02:18:22 -0800

    auth connected to backend (#50)
    Co-authored-by: Tony Jiang <tonyjiang@Tonys-MBP-2.lan>

--- frontend/byte-size/app/api/auth/[...nextauth]/authOptions.ts
+++ frontend/byte-size/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,4 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
+import axios from 'axios'
+const url = "http://127.0.0.1:8000"
 export const authOptions = {
     providers: [
       GoogleProvider({
@@ -7,6 +9,48 @@ export const authOptions = {
       }),
     ],
     callbacks: {
+        async session({session, token, user}) {
+            const user_email = session.user.email
+            const nameParts = session.user.name.split(" ")
+            const firstName = nameParts[0]
+            const lastName = nameParts[1]
+            const signUpUrl = url + "/auth/signUp"
+            const loginUrl = url + "/auth/login"
+            // signup
+            try {
+                const signUpResponse = await axios({
+                    method:"post",
+                    url: signUpUrl,
+                    data: {
+                        email: user_email,
+                        password: "password",
+                        firstName: firstName,
+                        lastName: lastName
+                    },
+                    headers: {
+                        "Content-Type": "application/json" // Set the content type to JSON
+                    }
+                })
+            } catch(error) {
+                console.log(error)
+            }
+            try {
+                const loginResponse = await axios({
+                    method:"post",
+                    url: loginUrl,
+                    data:{
+                        username: user_email,
+                        password: "password"
+                    },
+                    headers:{
+                        "Content-Type":"application/x-www-form-urlencoded"
+                    }
+                })
+                session.token = loginResponse.data.access_token
+                return session
+            } catch(error) {
+                console.log(error)
+            }
+        }
     },
-    debug: true
   };
