6808d321f3764923783ca9f50f48980fa0530d03
Tony Jiang <tonyjiang@Tonys-MBP-2.lan>
2023-12-08 23:26:24 -0800

    add a few more tests

--- frontend/byte-size/__tests__/index.test.js
+++ frontend/byte-size/__tests__/index.test.js
@@ -1,5 +1,5 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import UserHome from '../app/home/page'; // Adjust this path as per your project
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import UserHome from '../app/home/page';
 import '@testing-library/jest-dom';
 import fetchMock from 'jest-fetch-mock';
 import { useSession } from "next-auth/react";
@@ -26,7 +26,7 @@ describe('UserHome Tests', () => {
     const mockSubscribedRepos = [{ name: 'subscribed-repo1', url: 'http://subscribed-repo1.com' }];
     fetch.mockResponses(
       [JSON.stringify(mockSubscribedRepos), { status: 200 }],
-      [JSON.stringify([]), { status: 200 }] // Mock response for getAllRepos
+      [JSON.stringify([]), { status: 200 }]
     );
 
     render(<UserHome />);
@@ -39,7 +39,7 @@ describe('UserHome Tests', () => {
     useSession.mockReturnValue({ data: { token: 'fake-token' }, status: 'authenticated' });
     const mockRepos = [{ name: 'repo1', url: 'http://repo1.com' }];
     fetch.mockResponses(
-      [JSON.stringify([]), { status: 200 }], // Mock response for getSubbedRepos
+      [JSON.stringify([]), { status: 200 }],
       [JSON.stringify(mockRepos), { status: 200 }]
     );
 
@@ -47,5 +47,26 @@ describe('UserHome Tests', () => {
     await waitFor(() => {
       expect(screen.getByText('repo1')).toBeInTheDocument();
     });
+  });
+
+  it('subscribes to a repository correctly', async () => {
+    const mockSession = { data: { token: 'fake-token' }, status: 'authenticated' };
+    useSession.mockReturnValue(mockSession);
+
+    const mockRepos = [{ name: 'repo1', url: 'http://repo1.com' }];
+    const mockSubscribedRepos = [];
+
+    fetch.mockResponses(
+      [JSON.stringify(mockSubscribedRepos), { status: 200 }],
+      [JSON.stringify(mockRepos), { status: 200 }],
+      [JSON.stringify({}), { status: 200 }]
+    );
+
+    render(<UserHome />);
+
+    const subscribeButton = await screen.findByText('Subscribe');
+    fireEvent.click(subscribeButton);
+
+    expect(global.fetch).toHaveBeenCalledWith(expect.stringContaining('subscribeToRepo'), expect.anything());
   });
 });
